8dc36ff5ef3b50c3747d1a77afef22086d40803e
ydvavra <99927611+ydvavra@users.noreply.github.com>
2022-06-01 22:30:47 -0300

    rota carrinho

--- src/routes/main.js
+++ src/routes/main.js
@@ -6,5 +6,6 @@ const mainController = require('../controllers/mainController');
 router.get('/', mainController.index); 
 router.get('/registro', mainController.registro); 
 router.get('/produtos', mainController.produtos); 
+router.get('/carrinho', mainController.carrinho);
 
 module.exports = router;
