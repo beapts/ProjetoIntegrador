dbcaec17620ba7e94ecd466bb370068906ccacb8
ydvavra <99927611+ydvavra@users.noreply.github.com>
2022-06-07 20:01:25 -0300

    rota login atualizada

--- src/routes/main.js
+++ src/routes/main.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.index); 
-router.get('/registro', mainController.registro); 
+router.get('/login', mainController.login); 
 router.get('/produtos', mainController.produtos); 
 router.get('/carrinho', mainController.carrinho);
 router.get('/cadastro', mainController.cadastro);
