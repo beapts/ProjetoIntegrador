d43ee500c349997418f082dbb03836ffe628518e
ydvavra <99927611+ydvavra@users.noreply.github.com>
2022-07-17 22:32:53 -0300

    rotas atualizadas

--- src/routes/carrinhoRouter.js
+++ src/routes/carrinhoRouter.js
@@ -4,3 +4,5 @@ const router = express.Router();
 const carrinhoController = require('../controllers/carrinhoController');
 
 router.get('/carrinho', carrinhoController.carrinho);
+
+module.exports = router;
