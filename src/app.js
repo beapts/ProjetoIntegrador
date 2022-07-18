4bd4a3748e10f5041634460063c863bd1b1ecd6a
ydvavra <99927611+ydvavra@users.noreply.github.com>
2022-07-17 22:35:10 -0300

    rotas e controllers atualizadas

--- src/app.js
+++ src/app.js
@@ -11,8 +11,20 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 
-const mainRouter = require('./routes/main');  
+const cadastroRouter = require('./routes/cadastroRouter');  
+const carrinhoRouter = require('./routes/carrinhoRouter');  
+const homeRouter = require('./routes/homeRouter');  
+const listagemRouter = require('./routes/listagemRouter');  
+const loginRouter = require('./routes/loginRouter');  
+const painelRouter = require('./routes/painelRouter');  
+const produtoRouter = require('./routes/produtoRouter');  
 
-app.use('/', mainRouter);
+app.use('/cadastro', cadastroRouter);
+app.use('/carrinho', carrinhoRouter);
+app.use('/', homeRouter);
+app.use('/listagem', listagemRouter);
+app.use('/login', loginRouter);
+app.use('/painel', painelRouter);
+app.use('/produto', produtoRouter);
 
 module.exports = app;
