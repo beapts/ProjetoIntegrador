73373167aa5cdf0bcd74e833d562f92ba7f214a8
ydvavra <99927611+ydvavra@users.noreply.github.com>
2022-07-17 22:32:33 -0300

    rotas atualizadas

--- src/routes/cadastroRouter.js
+++ src/routes/cadastroRouter.js
@@ -4,3 +4,5 @@ const router = express.Router();
 const cadastroController = require('../controllers/cadastroController');
 
 router.get('/cadastro', cadastroController.cadastro);
+
+module.exports = router;
