f9bb4a8250fe6259f72e2357d2e4424e3828f535
ydvavra <99927611+ydvavra@users.noreply.github.com>
2022-07-17 22:34:16 -0300

    rotas atualizadas

--- src/routes/produtoRouter.js
+++ src/routes/produtoRouter.js
@@ -7,3 +7,5 @@ const produtoController = require('../controllers/produtoController');
 //fazer as rotas e as views pra cada produto separamente depois, atenção aos produtos do banco de dados (yohanna)
 
 router.get('/produto', produtoController.produto);
+
+module.exports = router;
