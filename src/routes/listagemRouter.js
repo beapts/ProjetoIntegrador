a96a8583fa18f96bc43d1fedcb8f76dee68dac35
ydvavra <99927611+ydvavra@users.noreply.github.com>
2022-07-17 22:33:20 -0300

    rotas atualizadas

--- src/routes/listagemRouter.js
+++ src/routes/listagemRouter.js
@@ -4,3 +4,5 @@ const router = express.Router();
 const listagemController = require('../controllers/listagemController');
 
 router.get('/listagem', listagemController.listagem);
+
+module.exports = router;
