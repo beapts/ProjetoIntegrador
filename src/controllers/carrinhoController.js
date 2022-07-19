63828f93c27271fbb86d652b22d937d89c0835bf
Beatriz Pietraroia <bia_pietraroia@hotmail.com>
2022-07-19 20:54:12 -0300

    correção rotas

--- src/controllers/carrinhoController.js
+++ src/controllers/carrinhoController.js
@@ -1,16 +1,16 @@
-<<<<<<< HEAD
+
 const carrinhoController = {
     carrinho: (req, res) => {
 		res.render('finalizarCompra');
 	}
 }
 
-=======
-const carrinhoController = {
+
+/* const carrinhoController = {
     carrinho: (req, res) => {
 		res.render('carrinho');
 	}
-}
+} */
 
->>>>>>> a196c3fdb13c0a1925585366bcc42bdf6032d31b
+
 module.exports = carrinhoController
--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
+
 const produtoController = {
     produto: (req, res) => {
 		res.render('produtos');
 	}
 }
 
-=======
-const produtoController = {
-    produto: (req, res) => {
-		res.render('produto');
-	}
-}
-
->>>>>>> a196c3fdb13c0a1925585366bcc42bdf6032d31b
 module.exports = produtoController
